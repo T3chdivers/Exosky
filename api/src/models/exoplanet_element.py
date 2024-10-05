c790e90baca19a5574aaaeac7fb0a7742667b06c
Dixen-Naxos <armanddailly@gmail.com>
2024-10-06 00:44:32 +0200

    Fix part 2

--- api/src/models/exoplanet_element.py
+++ api/src/models/exoplanet_element.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 @dataclass
 class ExoplanetElement:
     planet_id: str
-    ra: float
-    dec: float
+    x: float
+    y: float
+    z: float
     distance: float
