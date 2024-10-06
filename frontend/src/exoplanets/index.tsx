c92961f71edf39d2311b81e18d992f7e026d71e2
Loïc Vanden Bossche <l.vandenbossche.jouvet@gmail.com>
2024-10-06 08:35:29 +0200

    Added cache

--- frontend/src/exoplanets/index.tsx
+++ frontend/src/exoplanets/index.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react"
-import { Canvas } from "@react-three/fiber"
+import {useEffect, useState} from "react"
+import {Canvas} from "@react-three/fiber"
 import styles from "././index.module.css"
 import axios from "axios"
-import { Earth } from "./Earth"
-import { SelectionMenu } from "./SelectionMenu"
-import { Exoplanet } from "./Exoplanet"
+import {Earth} from "./Earth"
+import {SelectionMenu} from "./SelectionMenu"
+import {Exoplanet} from "./Exoplanet"
 
 export type ExoplanetDTO = {
   planet_id: string;
@@ -18,17 +18,24 @@ export function Exoplanets() {
   const [exoplanets, setExoplanets] = useState<ExoplanetDTO[] | undefined>();
 
   useEffect(() => {
+    if (localStorage.getItem("exoplanets")) {
+      setExoplanets(JSON.parse(localStorage.getItem("exoplanets") as string));
+      return;
+    }
+
     axios.get("https://exosky-api.dixen.fr/exoplanets/").then((response: any) => {
       setExoplanets(response.data as ExoplanetDTO[]);
+
+      localStorage.setItem("exoplanets", JSON.stringify(response.data));
     });
   }, []);
 
   return (
     <div id="canvas-container" className={styles.canvasContainer}>
       <Canvas camera={{far: 5000, position: [6, 5, -2]}} className={styles.canvas}>
-        <Earth />
-        { !!exoplanets?.length && exoplanets.map((exoplanet: ExoplanetDTO) => <Exoplanet exoplanet={exoplanet}/>) }
-        <ambientLight intensity={0.1} />
+        <Earth/>
+        {!!exoplanets?.length && exoplanets.map((exoplanet: ExoplanetDTO) => <Exoplanet exoplanet={exoplanet}/>)}
+        <ambientLight intensity={0.1}/>
       </Canvas>
       <SelectionMenu exoplanets={exoplanets}/>
     </div>
